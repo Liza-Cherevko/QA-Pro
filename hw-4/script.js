9228fd27a17602dc97f2a36fec81a8afc3251470
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-12-13 14:44:16 +0200

    hw-4

--- hw-4/script.js
+++ hw-4/script.js
@@ -1,11 +1,31 @@
 let measurement = prompt('Введіть перше значення на вибір км, години, кілограми');
+
 let quantity = +prompt('Введіть кількість');
 
-if (measurement === 'км') {
-    alert(quantity + 'км ' + '= ' + (quantity * 1000) + 'метрів')
-} else if (measurement === 'год') {
-    alert(quantity + 'год ' + '= ' + (quantity * 60) + 'хвилин')
-} else if (measurement === 'кг') {
-    alert(quantity + 'кг ' + '= ' + (quantity * 1000) + 'грамів')
-} else alert('невірне значення')
+switch (measurement) {
+    case measurement = 'км': alert(quantity + 'км ' + '= ' + (quantity * 1000) + 'метрів')
+        break;
+    case measurement = 'год': alert(quantity + 'год ' + '= ' + (quantity * 60) + 'хвилин')
+        break;
+    case measurement = 'кг': alert(quantity + 'кг ' + '= ' + (quantity * 1000) + 'грамів')
+        break;
+}
 
+// типу з валідацією але після повторного запиту результат - невірне значення
+// let measurement = prompt('Введіть перше значення на вибір км, години, кілограми');
+
+// if (measurement !== 'км' && measurement !== 'год' && measurement !== 'кг') {
+//     prompt('Введіть перше значення на вибір км, години, кілограми')
+// }
+// let quantity = +prompt('Введіть кількість');
+
+// switch (measurement) {
+//     case measurement = 'км': alert(quantity + 'км ' + '= ' + (quantity * 1000) + 'метрів')
+//         break;
+//     case measurement = 'год': alert(quantity + 'год ' + '= ' + (quantity * 60) + 'хвилин')
+//         break;
+//     case measurement = 'кг': alert(quantity + 'кг ' + '= ' + (quantity * 1000) + 'грамів')
+//         break;
+
+//     default: alert('невірне значення')
+// }
