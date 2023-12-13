a5b85dd33d7839c1ccd851103a42bfe78baab99d
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-12-13 13:24:04 +0200

    hw-3

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,13 +1,16 @@
 
 let question = +prompt('Скільки вам років?')
 let validate = Math.sign(question) == -1 || Math.sign(question) == 0
+let ageValidFirst = question == 1 || question == 21 || question == 31 || question == 41 || question == 51 || question == 61 || question == 71 || question == 81 || question == 91
+let ageValidSecond = question > 1 && question <= 4 || question > 21 && question <= 24 || question > 31 && question <= 34 || question > 41 && question <= 44 || question > 51 && question <= 54 || question > 61 && question <= 64 || question > 71 && question <= 74 || question > 81 && question <= 84 || question > 91 && question <= 94
 if (validate) {
     console.log('невірне значення')
-} else if (question == 1) {
+} else if (ageValidFirst) {
     console.log('Вам ' + question + ' рік');
-} else if (question > 1 && question <= 4) {
+} else if (ageValidSecond) {
     console.log('Вам ' + question + ' роки');
-} else console.log('Вам ' + question + ' років');
+}
+else console.log('Вам ' + question + ' років');
 
 
 
