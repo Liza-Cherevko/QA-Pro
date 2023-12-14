ae56c3c2f04191b9cb80dc168e3f9fd66e4ca132
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-12-14 17:00:38 +0200

    add default value

--- hw-4/script.js
+++ hw-4/script.js
@@ -1,5 +1,11 @@
 let measurement = prompt('Введіть перше значення на вибір км, години, кілограми');
 
+
+if (measurement !== 'км' && measurement !== 'год' && measurement !== 'кг') {
+    measurement = prompt('Виберіть одне із значень км, години, кілограми');
+}
+console.log(measurement)
+
 let quantity = +prompt('Введіть кількість');
 
 switch (measurement) {
@@ -9,23 +15,6 @@ switch (measurement) {
         break;
     case measurement = 'кг': alert(quantity + 'кг ' + '= ' + (quantity * 1000) + 'грамів')
         break;
+    default: alert('невірне значення')
 }
 
-// типу з валідацією але після повторного запиту результат - невірне значення
-// let measurement = prompt('Введіть перше значення на вибір км, години, кілограми');
-
-// if (measurement !== 'км' && measurement !== 'год' && measurement !== 'кг') {
-//     prompt('Введіть перше значення на вибір км, години, кілограми')
-// }
-// let quantity = +prompt('Введіть кількість');
-
-// switch (measurement) {
-//     case measurement = 'км': alert(quantity + 'км ' + '= ' + (quantity * 1000) + 'метрів')
-//         break;
-//     case measurement = 'год': alert(quantity + 'год ' + '= ' + (quantity * 60) + 'хвилин')
-//         break;
-//     case measurement = 'кг': alert(quantity + 'кг ' + '= ' + (quantity * 1000) + 'грамів')
-//         break;
-
-//     default: alert('невірне значення')
-// }
