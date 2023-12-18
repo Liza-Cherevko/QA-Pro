f64d66324fee308ae940127102ee9b0593b6241f
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-12-18 19:08:48 +0200

    fix hw-5.2

--- hw-5.2/script.js
+++ hw-5.2/script.js
@@ -1,5 +1,22 @@
-for (let i = 0; i <= 100; i++) { 
-    if (i % 3 !== 0) { 
-        console.log(i) 
+// let sum = 0;
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 3 !== 0) {
+//         let num = i;
+//         sum += num;
+//         console.log(sum)
+//     }
+
+// }
+function calculateTotal() {
+    let sum = 0;
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 !== 0) {
+            sum += i;
+        }
     }
+    console.log(sum)
+    return sum;
 }
+calculateTotal()
+
+
