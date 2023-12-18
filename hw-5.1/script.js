deaacf4f609f10ba7196e748124f098c1d3d3ade
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2023-12-18 12:17:53 +0200

    hw5.1

---
+++ hw-5.1/script.js
@@ -0,0 +1,16 @@
+
+let star = "*";
+let str = " ";
+let lines = 5;
+// #1
+while (star.length <= 5) {
+    console.log(star);
+    star += "*";
+}
+
+// #2
+// for (let i = 0; i < lines; i++) {
+//     str += star;
+//     console.log(str);
+// }
+
