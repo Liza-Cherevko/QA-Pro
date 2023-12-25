562a4246d0c37165203422a08e7a7c271b10fca4
Liza <cherevkoelp@gmail.com>
2023-12-25 18:07:09 +0200

    hw-7.2

--- hw-7.2/script.js
+++ hw-7.2/script.js
@@ -1,54 +1,66 @@
 let services = {
 
-	"стрижка": 60,
+	"стрижка": "60 грн",
 
-	"гоління": 80,
+	"гоління": "80 грн",
 
-	"Миття голови": 100
+	"Миття голови": "100 грн"
 
 };
 
 services.newProperty = function (propertyName, propertyValue) {
-    this[propertyName] = propertyValue;
+    this[propertyName] = propertyValue + ' грн';
     console.log(services)
   };
 
 services.getPrice=function(){
-    let sum = 0;
-    for (let key in services) {
-      if (typeof services[key] == 'number') {
-         sum += services[key];
+  let total = 0;
+    for (let key in this) {
+      if (this.hasOwnProperty(key)) {
+        let priceString = this[key];
+        let price = parseFloat(priceString.toString().replace(' грн', ''));
+        if (!isNaN(price)) {
+          total += price;
+        }
       }
     }
-    
-    return sum;
-}
+   return total;
+  }
+
 services.minPrice = function(){
     minPrice = Infinity;
     for (let key in services) {
         let priceString = services[key];
-        if (priceString < minPrice) {
-            minPrice = priceString;
+        let price = parseFloat(priceString.toString().replace(' грн', ''));
+        if (price < minPrice) {
+            minPrice = price;
           }
     }
 
     return minPrice;
   }
   services.maxPrice = function(){
-    maxPrice = -Infinity;
-    for (let key in services) {
-        let priceString = services[key];
-        if (priceString > maxPrice) {
-            maxPrice = priceString;
-          }
-    }
+    var maxPrice = -Infinity;
 
+    for (var key in this) {
+      if (this.hasOwnProperty(key)) {
+        let priceString = this[key];
+        let price = parseFloat(priceString.toString().replace(' грн', ''));
+        if (!isNaN(price) && price > maxPrice) {
+          maxPrice = price;
+        }
+      }
+    }
     return maxPrice;
   }
 
 
+
+
+
 console.log(services.getPrice())
-console.log(services.newProperty('Покраска', 20))
-console.log(services.newProperty('Кератин', 700))
+services.newProperty('Покраска', '20')
+services.newProperty('Кератин', '700')
+console.log(services)
 console.log(services.minPrice())
 console.log(services.maxPrice())
