140e1c2e0fa2edccf90b786540daca13c0e13306
Liza-Cherevko <71131183+Liza-Cherevko@users.noreply.github.com>
2024-01-09 11:33:25 +0200

    hw-9.2

--- hw-9.2/script.js
+++ hw-9.2/script.js
@@ -1,24 +1,26 @@
 let arr = [
     {
-        userName:"Test",
-        lastName:"Test",
-        email:"test.test@gmail.com"
+        userName: "Test",
+        lastName: "Test",
+        email: "test.test@gmail.com"
     },
     {
-        userName:"Dmitro",
-        lastName:"Porohov",
-        email:"dmitro.porohov@yahoo.com"
+        userName: "Dmitro",
+        lastName: "Porohov",
+        email: "dmitro.porohov@yahoo.com"
     },
     {
-        userName:"Andrii",
-        lastName:"",
-        email:"andrii@mail.ru" // Нам такі не підходять
+        userName: "Andrii",
+        lastName: "",
+        email: "andrii@mail.ru" // Нам такі не підходять
     },
 ];
 
-let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-let filteredEmails = arr.filter(function (obj) {
-    return (obj.email && emailRegex.test(obj.email) && (obj.email.endsWith("@gmail.com") || obj.email.endsWith("@yahoo.com")));
-
+var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+var validEmails = arr.filter(function (obj) {
+    return (obj.email.match(emailRegex) && (obj.email.includes('@gmail.com') || obj.email.includes('@yahoo.com')));
+}).map(function (obj) {
+    return obj.email;
 });
-console.log(filteredEmails);
+
+console.log(validEmails);
